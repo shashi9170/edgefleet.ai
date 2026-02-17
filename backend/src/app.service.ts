792ec9a439cec2315b9d0685939a47cea020f7d0
Shashi Prakash <157964187+shashi9170@users.noreply.github.com>
2026-02-17 18:30:20 +0530

    Update app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -26,4 +26,28 @@ export class AppService {
         console.log(`count -- ${cnt} `);
         return { success: true }
     }
+
+    printLocations(payload: any) {
+
+    if (!payload?.locations || !Array.isArray(payload.locations)) {
+      console.log("No locations received");
+      return { success: false };
+    }
+
+        console.log(`Total Locations: ${payload.locations.length}`);
+        console.log("--------------------------------------------------");
+        payload.locations.forEach((loc: any, index: number) => {
+        console.log(`Location #${index + 1}`);
+        console.log(`Employee ID : ${loc.emp_id}`);
+        console.log(`Latitude    : ${loc.latitude}`);
+        console.log(`Longitude   : ${loc.longitude}`);
+        console.log(`Timestamp   : ${loc.timestamp}`);
+        console.log(`Recorded At : ${loc.recorded_at}`);
+        console.log(`Address     : ${loc.address}`);
+        console.log(`Device Type : ${loc.device_type}`);
+        console.log("--------------------------------------------------");
+    });
+
+    return { success: true };
+  }
 }
