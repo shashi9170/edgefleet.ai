dc1d3d869e85585bfb7d9c8d5f0d4140f83adbc0
Shashi Prakash <157964187+shashi9170@users.noreply.github.com>
2026-02-17 18:32:11 +0530

    Add DTOs for single location and batch locations

--- backend/src/location.dto.ts
+++ backend/src/location.dto.ts
@@ -6,3 +6,17 @@ export class LocationDto {
 export class CountDto{
   cnt: number
 }
+
+export class SingleLocationDto {
+  emp_id: string;
+  latitude: number;
+  longitude: number;
+  timestamp: number;
+  recorded_at: string;
+  address: string;
+  device_type: string;
+}
+
+export class LocationsBatchDto {
+  locations: SingleLocationDto[];
+}
